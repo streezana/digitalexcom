192110787499e05596d65bc7838b75156108d673
streezana <luolala@yandex.ru>
2024-02-09 12:37:52 +0300

    Changed host = "https://hopnextgames-api.onrender.com"

--- utils/paths.js
+++ utils/paths.js
@@ -1,5 +1,5 @@
-const host = `http://localhost:${process.env.PORT}`
-// const host = "https://hopnextgames-api.onrender.com"
+const host = "https://hopnextgames-api.onrender.com"
+//const host = `http://localhost:${process.env.PORT}`
 // const host = "http://localhost:8000"
 module.exports = {
   host
