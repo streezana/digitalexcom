2ccd36973bd699763127ac028a4ac9425c31abab
streezana <luolala@yandex.ru>
2024-02-09 20:54:28 +0300

    Changed (!req.body.bookImage) to (!req.file)

--- controllers/books.js
+++ controllers/books.js
@@ -37,9 +37,12 @@ const getBook = async (req, res) => {
         if (!req.body.content) {
           errors.content = { message: "Пожалуйста, введите текст книги" };
         }
-        if (!req.body.bookImage) {
-          errors.bookImage = { message: "Пожалуйста, загрузите изображение" };
+        if (!req.file) {
+           errors.bookImage = { message: "Пожалуйста, загрузите изображение" };
         }
+        // if (!req.body.bookImage) {
+        //   errors.bookImage = { message: "Пожалуйста, загрузите изображение" };
+        // }
         if (Object.keys(errors).length > 0) {
           fs.unlinkSync(`./assets/${req.file.filename}`)
           console.log('img removed / ' + req.file.filename)
